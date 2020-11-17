0ca9926b7af78b1b5be3a1580a1eb5fae9fb95da
1190100 <1190100@isep.ipp.pt>
2020-11-17 17:03:07 +0000

    First @vue/cli interaction

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+<section>
+<h2> My friends </h2>
+<ul>
+<li>   </li>
+</ul>
+</section>
+</template>
+
+
+
+<script>
+export default {
+
+data() {
+    return {
+        friends: [
+            {
+                id: "Manuel",
+                name: 'Manuel Lorenz',
+                phone: '0123 45675 90',
+                email: 'manuel@localhost.com'
+            },
+            {
+                id: "Julie",
+                name: 'Julie Jones',
+                phone: '0112 45674 90',
+                email: 'juliel@localhost.com'
+            }
+        ]
+    }
+},
+
+
+
+}
+
+
+</script>
